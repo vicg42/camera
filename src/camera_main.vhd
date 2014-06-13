b890421f2d8e2905b517a73dacfd1f5e9c9995f3
vicg42 <vicg42@gmail.com>
2014-06-13 17:45:48 +0300

    src/

--- src/camera_main.vhd
+++ src/camera_main.vhd
@@ -16,15 +16,23 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 library work;
+use work.reduce_Pack.all;
 use work.vicg_common_pkg.all;
 use work.clocks_pkg.all;
+use work.ccd_vita25K_pkg.all;
 
 entity camera_main is
 port(
 --------------------------------------------------
 --Технологический порт
 --------------------------------------------------
-pin_out_TP          : out   std_logic_vector(2 downto 0);
+pin_out_TP          : out   std_logic_vector(15 downto 0);
+
+--------------------------------------------------
+--CCD
+--------------------------------------------------
+pin_in_ccd            : in   TCCD_PortIN;
+pin_out_ccd           : out  TCCD_PortOUT;
 
 --------------------------------------------------
 --Reference clock
@@ -44,10 +52,24 @@ p_in_clk   : in    TRefClkPinIN
 );
 end component;
 
+component ccd_vita25K is
+port(
+p_in_ccd   : in   TCCD_PortIN;
+p_out_ccd  : out  TCCD_PortOUT;
+
+p_out_vd     : out std_logic_vector(160 - 1 downto 0);
+p_out_vd_clk : out std_logic;
+
+p_in_refclk : in   std_logic;
+p_in_ccdclk : in   std_logic;
+p_in_rst    : in   std_logic
+);
+end component;
 
 signal i_rst                            : std_logic;
 signal g_usrclk                         : std_logic_vector(7 downto 0);
-
+signal i_vd                             : std_logic_vector(160 - 1 downto 0);
+signal i_vd_clk                         : std_logic;
 
 signal tst_cnt : std_logic_vector(2 downto 0);
 
@@ -65,19 +87,40 @@ p_out_gclk => g_usrclk,
 p_in_clk   => pin_in_refclk
 );
 
+
+--***********************************************************
+--Установка частот проекта:
+--***********************************************************
+m_ccd : ccd_vita25K
+port map(
+p_in_ccd  => pin_in_ccd,
+p_out_ccd => pin_out_ccd,
+
+p_out_vd     => i_vd,
+p_out_vd_clk => i_vd_clk,
+
+p_in_refclk => g_usrclk(0),
+p_in_ccdclk => g_usrclk(1),
+p_in_rst    => i_rst
+);
+
 --***********************************************************
 --Технологический порт
 --***********************************************************
-pin_out_TP <= tst_cnt;--(others => '0');
+--pin_out_TP <= tst_cnt;--(others => '0');
+--
+--process(i_rst, g_usrclk(0))
+--begin
+--  if i_rst = '1' then
+--    tst_cnt <= (others => '0');
+--  elsif rising_edge(g_usrclk(0)) then
+--    tst_cnt <= tst_cnt;
+--  end if;
+--end process;
 
-process(i_rst, g_usrclk(0))
-begin
-  if i_rst = '1' then
-    tst_cnt <= (others => '0');
-  elsif rising_edge(g_usrclk(0)) then
-    tst_cnt <= tst_cnt;
-  end if;
-end process;
+gen_tp : for i in 0 to (16 - 1) generate
+pin_out_TP(i) <= OR_reduce(i_vd((10 * (i + 1)) - 1 downto (10 * i)));
+end generate;
 
 --END MAIN
 end architecture;
