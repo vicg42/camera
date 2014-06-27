413e89eb1091170f5833a1f4948a1a1e3577cb33
vicg42 <vicg42@gmail.com>
2014-06-27 13:49:58 +0300

    src/vout.vhd --VGA Test Color Pattern - OK! ADV7123 test control - OK!

--- src/vout.vhd
+++ src/vout.vhd
@@ -23,7 +23,7 @@ p_out_video   : out  TVout_pinout;
 
 p_in_fifo_do  : in   std_logic_vector(31 downto 0);
 p_out_fifo_rd : out  std_logic;
-p_out_fifo_empty : in  std_logic;
+p_in_fifo_empty : in  std_logic;
 
 --System
 p_in_clk      : in   std_logic;
@@ -52,6 +52,7 @@ end component;
 signal i_video_vs           : std_logic;
 signal i_video_hs           : std_logic;
 signal i_video_den          : std_logic;
+signal i_cnt                : unsigned(9 downto 0) := (others => '0');
 
 
 --MAIN
@@ -73,19 +74,37 @@ p_in_clk    => p_in_clk,
 p_in_rst    => p_in_rst
 );
 
-
-p_out_video.vga_dr <= p_in_fifo_do(10 - 1 downto 0);
-p_out_video.vga_dg <= p_in_fifo_do(10 - 1 downto 0);
 p_out_video.vga_db <= p_in_fifo_do(10 - 1 downto 0);
+p_out_video.vga_dg <= p_in_fifo_do(10 - 1 downto 0);
+p_out_video.vga_dr <= p_in_fifo_do(10 - 1 downto 0);
 p_out_video.vga_hs <= i_video_hs;
 p_out_video.vga_vs <= i_video_vs;
 
+--Control DAC (ADV7123)
 p_out_video.dac_blank_n <= i_video_den;
-p_out_video.dac_sync_n  <= i_video_hs;
-p_out_video.dac_psave_n <= '1'; --Power Down OFF
-p_out_video.dac_clk     <= p_in_clk;
+p_out_video.dac_sync_n  <= '0';
+p_out_video.dac_psave_n <= '1';--Power Down OFF
+p_out_video.dac_clk     <= not p_in_clk;
 
 p_out_fifo_rd <= i_video_den;
+
+
+--gen : for i in 0 to p_out_video.vga_db'length - 1 generate
+--p_out_video.vga_db(i) <= '1' when i_cnt(7) = '1' else '0';
+--p_out_video.vga_dg(i) <= '1' when i_cnt(8) = '1' else '0';
+--p_out_video.vga_dr(i) <= '1' when i_cnt(9) = '1' else '0';
+--end generate;
+--
+--process(p_in_clk)
+--begin
+--  if rising_edge(p_in_clk) then
+--    if i_video_hs = '0' then
+--      i_cnt <= (others => '0');
+--    else
+--      i_cnt <= i_cnt + 1;
+--    end if;
+--  end if;
+--end process;
 
 
 --END MAIN
