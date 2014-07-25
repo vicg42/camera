ca9b604168f24ef59a4ab1927810360d9b35a3bd
vicg42 <vicg42@gmail.com>
2014-07-25 15:15:14 +0300

    script/ updata_ngc.bat -fix bug

--- script/updata_ngc.bat
+++ script/updata_ngc.bat
@@ -1,6 +1,6 @@
 rem Ищем каталог core_gen и копируем из нее все файлы (*.ngc,*.mif ) в каталог проекта ISE(..\ise\prj)
 
 cd d:\Work\Yansar\camera\ise\core_gen\
-for /R  %%f in ( *.ngc *.mif) do xcopy "%%f" ..\ise\prj /y
+for /R  %%f in ( *.ngc *.mif) do xcopy "%%f" ..\prj /y
 
 dir
