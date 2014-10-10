65a44513da5fd295c43f8e0cf279e445a908464e
vicg42 <vicg42@gmail.com>
2014-10-10 16:29:24 +0300

    srcript/

--- script/jtag_download.cmd
+++ script/jtag_download.cmd
@@ -1,6 +1,6 @@
 setMode -bs
 setCable -p auto
 identify
-assignfile -p 1 -file d:\Work\Yansar\camera\firmware\test_ccd_main.bit
+assignfile -p 1 -file d:\Work\Yansar\camera\firmware\camera_main.bit
 program -p 1
 quit
